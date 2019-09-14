a597891e3bda0ed5c9bab577558fa10414b11ada
Laura <l.maya10@uniandes.edu.co>
2019-09-14 15:45:51 -0500

    Datos funcionan

--- app.js
+++ app.js
@@ -18,23 +18,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "front/build")));
 
-app.get('*', function(_, res) {
-  res.sendFile(path.join(__dirname, './front/build/index.html'), function(err) {
-    if (err) {
-      res.status(500).send(err)
-    }
-  })
-})
-
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,15 @@
 var express = require("express");
 var router = express.Router();
-const fileUpload = require('express-fileupload');
-const cloudinary = require('cloudinary');
-//------------------------------------------------------------------------------------------------
-// CONFIGURATIONS
-//------------------------------------------------------------------------------------------------
 
-//Cloudinary conf
-cloudinary.config({
-  cloud_name: 'drfggfn8f',
-  api_key: '694159519369718',
-  api_secret: 'jwkPS4klzsaEHd-eNf8HIiPkHW8'
-});
-
-
-
-//Conf for receiving formData (files)
-router.use(fileUpload({
-  useTempFiles: true
-}));
-
-//Conf for sending access contro allow headers
-router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-//------------------------------------------------------------------------------------------------
-// SERVICES
-//------------------------------------------------------------------------------------------------
-
-//SERVICE: This method receives an image files and uploads it to cloudinary
-router.post("/uploadimage", function (req, res, next) {
-  let image = req.files.image;
-  let body = req.body;
-
-  cloudinary.v2.uploader.upload(image.tempFilePath, {
-    "folder": "/macco/bolsos"
-  }, (err, res2) => {
-    if (err) throw err;
-    res.send(res2.url);
-  });
-});
-
+/*GET home page*/
 router.get("/productos", function(req, res, next) {
   const productos = [
-    {id: 1, nombre: "Bolso1", precio:20000},
-    {id: 2, nombre: "Bolso2", precio:25000},
-    {id: 3, nombre: "Bolso3", precio:30000},
+    {id: 1, nombre:"Bolso1", precio:20000},
+    {id: 2, nombre:"Bolso2", precio:25000},
+    {id: 3, nombre:"Bolso3", precio:30000}
   ];
   res.json(productos);
+
 });
 
 module.exports = router;
