a14977e3c9b5168d298033ccaef08b4634c538bf
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-09-15 19:12:58 -0500

    Why?

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// sguzmanm: Why are you using automated testing and why didn´t you do it for your other components?
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
