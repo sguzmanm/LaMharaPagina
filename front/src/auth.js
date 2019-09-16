6934988683ea96aaaf6ea233d75991e9e5de551f
Sergio Guzmán Mayorga <s.guzmanm98@gmail.com>
2019-09-15 19:04:58 -0500

    Why is auth like this?

--- front/src/auth.js
+++ front/src/auth.js
@@ -1,5 +1,8 @@
+// sguzmanm: Why are you importing React?
 import React from "react";
 
+// sguzmanm: Why are you using a singleton right here? Wouldn`t it be better to use localStorage or React State instead?
+// Because you are giving a single point of failure to your app`s authentication
 class Auth{
   constructor(){
     this.authenticated=false;
